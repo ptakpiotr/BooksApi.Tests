3a3d317dba24b1947907792156b06a72a4ec2677
ptakpiotr <piter020@outlook.com>
2022-03-30 00:05:45 +0200

    Added Unit Tests for BooksAPI

---
+++ BooksRepoTests.cs
@@ -0,0 +1,68 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using WebAPI.Controllers;
+using WebAPI.Data;
+using WebAPI.Models;
+using WebAPI.Models.DTOs;
+using Xunit;
+
+namespace LibraryAPI.Tests
+{
+    public class BooksRepoTests
+    {
+        private readonly Mock<IBookRepo> _repo;
+        private readonly IMapper _mockMapper;
+
+        public BooksRepoTests()
+        {
+            _repo = new Mock<IBookRepo>();
+            _mockMapper = new MapperConfiguration(opts =>
+            {
+                opts.CreateMap<BookAddDTO,BookModel>().ReverseMap();
+                opts.CreateMap<BookModel, BookReadDTO>();
+            }).CreateMapper();
+        }
+
+        [Fact]
+        public void GetAllBooks_Should_Return()
+        {
+            //arrange
+            _repo.Setup(x => x.GetAllBooks()).Returns(MockingHelpers.GetAllBooksMoq);
+            BooksController bookController = new BooksController(null, _repo.Object, _mockMapper);
+            //act
+            IActionResult res = bookController.GetAllBooks();
+            //assert
+            Assert.IsType<OkObjectResult>(res);
+        }
+
+        [Theory]
+        [InlineData("e57160b9-ee27-493d-a5ab-5ed045079cd6")]
+        public void GetOneBook_Should_Return(Guid id)
+        {
+            //arrange
+            _repo.Setup(x => x.GetOneBook(id)).Returns(MockingHelpers.GetOneBookMoq);
+            BooksController booksController = new BooksController(null,_repo.Object, _mockMapper);
+            //act 
+            IActionResult res = booksController.GetOneBook(id);
+            //assert
+            Assert.IsType<NotFoundResult>(res);
+        }
+
+        [Fact]
+        public void UpdateBook_Should_Return()
+        {
+            //arrange
+            _repo.Setup(x => x.UpdateBook(MockingHelpers.testBook)).Verifiable();
+            BooksController booksController = new BooksController(null, _repo.Object, _mockMapper);
+            //act
+            IActionResult res = booksController.EditBook(Guid.NewGuid(), new BookAddDTO());
+
+            Assert.IsType<NotFoundResult>(res);
+            
+        }
+    }
+}
---
+++ MockingHelpers.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebAPI.Models;
+
+namespace LibraryAPI.Tests
+{
+    public class MockingHelpers
+    {
+        private static List<BookModel> allBooks = new List<BookModel>()
+        {
+            new BookModel()
+            {
+                Author="jack@jack.com",
+                Book="jack book",
+                Country=null,
+                CountryId = 1,
+                Email="jack@jack.com",
+                Id=Guid.NewGuid(),
+                Pages = 1
+            },
+            new BookModel()
+            {
+                Author="jack2@jack.com",
+                Book="jack2 book",
+                Country=null,
+                CountryId = 1,
+                Email="jack2@jack.com",
+                Id=Guid.NewGuid(),
+                Pages = 2
+            },
+            new BookModel()
+            {
+                Author="jack3@jack.com",
+                Book="jack3 book",
+                Country=null,
+                CountryId = 1,
+                Email="jack3@jack.com",
+                Id=Guid.NewGuid(),
+                Pages = 3
+            }
+        };
+        public static BookModel testBook = new BookModel()
+        {
+            Author = "jim@jim.com",
+            Book = "Jim\'s book",
+            Country = null,
+            CountryId = 1,
+            Email = "jim@jim.com",
+            Id = Guid.NewGuid(),
+            Pages = 100
+        };
+        public static List<BookModel> GetAllBooksMoq() => allBooks;
+
+        public static BookModel GetOneBookMoq(Guid id) => allBooks.FirstOrDefault(b => b.Id == id);
+
+        public static void AddBookMoq(BookModel md)
+        {
+            allBooks.Add(md);
+        }
+        public static void DeleteBookMoq(BookModel md)
+        {
+            allBooks.Remove(md);
+        }
+        public static void UpdateBookMoq(BookModel md)
+        {
+        }
+    }
+}
